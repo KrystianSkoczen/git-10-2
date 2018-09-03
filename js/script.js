9b8573c51a41d3f62d0ea71b8ba2027e92f66db4
skoczenkrystian <skoczenkrystian@gmail.com>
2018-09-03 19:51:58 +0200

    add new

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@
 	results.insertAdjacentHTML('beforeend', fullSlideList);
 
 })();
+
+	var flkty = new Flickity( '.main-carousel', {
+	    cellAlign: 'left',
+	    draggable: true,
+	    prevNextButtons: true,
+	    pageDots: false,
+	    hash: true
+	});
+
+	var progressBar = document.querySelector('.progress-bar')
+
+	flkty.on( 'scroll', function( progress ) {
+	 	progress = Math.max( 0, Math.min( 1, progress ) );
+		progressBar.style.width = progress * 100 + '%';
+	});
+
+	$('.reset').on( 'click', function() {
+	    flkty.select(0);
+});
